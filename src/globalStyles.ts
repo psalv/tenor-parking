e87170939adc10471a44ad1d5bb16ee1c6ab4a55
Paul Salvatore <paulanthonysalvatore@gmail.com>
2026-02-26 16:08:34 -0500

    fix path

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -4,10 +4,10 @@ import { colors } from "./colors";
 export const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: 'Gambetta';
-    src: url('/fonts/Gambetta-Regular.eot');
-    src: url('/fonts/Gambetta-Regular.eot?#iefix') format('embedded-opentype'),
-         url('/fonts/Gambetta-Regular.otf') format('opentype'),
-         url('/fonts/Gambetta-Regular.ttf') format('truetype');
+    src: url('./fonts/Gambetta-Regular.eot');
+    src: url('./fonts/Gambetta-Regular.eot?#iefix') format('embedded-opentype'),
+         url('./fonts/Gambetta-Regular.otf') format('opentype'),
+         url('./fonts/Gambetta-Regular.ttf') format('truetype');
     font-weight: normal;
     font-style: normal;
     font-display: swap;
@@ -15,7 +15,7 @@ export const GlobalStyles = createGlobalStyle`
 
   @font-face {
     font-family: 'Roboto';
-    src: url('/fonts/Roboto-Regular.ttf') format('truetype');
+    src: url('./fonts/Roboto-Regular.ttf') format('truetype');
     font-weight: normal;
     font-style: normal;
     font-display: swap;
@@ -23,7 +23,7 @@ export const GlobalStyles = createGlobalStyle`
 
   @font-face {
     font-family: 'Roboto';
-    src: url('/fonts/Roboto-Bold.ttf') format('truetype');
+    src: url('./fonts/Roboto-Bold.ttf') format('truetype');
     font-weight: 500;
     font-style: normal;
     font-display: swap;
@@ -39,7 +39,7 @@ export const GlobalStyles = createGlobalStyle`
     font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    background-image: url('/bg_image.jpg');
+    background-image: url('./bg_image.jpg');
     background-size: cover;
     background-position: center;
     background-attachment: fixed;
